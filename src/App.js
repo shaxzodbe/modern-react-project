094578a2a2f1fff3c17843cdf30daa4fffcca794
shaxzodbe <sulaymanovshaxzod@gmail.com>
2021-06-07 11:42:57 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Showcase from './components/Showcase'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Showcase />
+    </>
+  );
+}
+
+export default App;
+  
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import logo from "../images/logo.svg";
+
+export class Header extends Component {
+  render() {
+    return (
+      <>
+        <header className="header">
+          <div className="logo">
+            <img src={logo} alt="Loopstudios" />
+          </div>
+
+          <nav>
+            <ul>
+              <li>
+                <button>About</button>
+              </li>
+              <li>
+                <button>Careers</button>
+              </li>
+              <li>
+                <button>Events</button>
+              </li>
+              <li>
+                <button>Products</button>
+              </li>
+              <li>
+                <button>Support</button>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="menu-btn">
+            <div className="bar bar-1"></div>
+            <div className="bar bar-2"></div>
+            <div className="bar bar-3"></div>
+          </div>
+        </header>
+      </>
+    );
+  }
+}
+
+export default Header;
